b00fed58c8db33e64ca3b1ee2a760bdaa9f48c83
Tom Yang <xy2286@columbia.edu>
2020-08-12 21:56:30 -0400

    add router and aws connection in front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import "./App.css";
+import Routes from "./Routes";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Navbar collapseOnSelect>
+        <Navbar.Brand>
+          <Link to="/">Scratch</Link>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+
+        <Navbar.Collapse className="justify-content-end">
+          <Nav>
+            <LinkContainer to="/signup">
+              <Nav.Link>Signup</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes />
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "yxy-demo-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://95ej56d2pg.execute-api.us-east-2.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_IGqy1TPui",
+      APP_CLIENT_ID: "7d2nthvclm8eva7bf217a9fm81",
+      IDENTITY_POOL_ID: "us-east-2:0b301fca-b4e2-454f-a0d9-b47c35ab6ee1"
+    }
+  };
