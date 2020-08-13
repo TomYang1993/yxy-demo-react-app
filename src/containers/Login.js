d43bb1e89c861f9bbd278aee963d904639aaea2d
Tom Yang <xy2286@columbia.edu>
2020-08-12 22:34:30 -0400

    add login ui

---
+++ src/containers/Login.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import Form from 'react-bootstrap/Form'
+import "./Login.css";
+import { Auth } from "aws-amplify";
+
+
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+  
+    try {
+      await Auth.signIn(email, password);
+      alert("Logged in");
+    } catch (e) {
+      alert(e.message);
+    }
+  }
+
+  return (
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
+        <Form.Group controlId="email" bsSize="large">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="password" bsSize="large">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            type="password"
+          />
+        </Form.Group>
+        <Button block bsSize="large" disabled={!validateForm()} type="submit">
+          Login
+        </Button>
+      </form>
+    </div>
+  );
+}
