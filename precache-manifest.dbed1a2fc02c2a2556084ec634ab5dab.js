2b0e80c813bd2318f2e85acbf8b2850e6ef68f92
Tom Yang <xy2286@columbia.edu>
2020-08-21 15:04:50 -0400

    Updates

---
+++ precache-manifest.dbed1a2fc02c2a2556084ec634ab5dab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5822880785a6c8db8501cd96be4ab236",
+    "url": "/yxy-demo-react-app/index.html"
+  },
+  {
+    "revision": "75bac312a74f004faf97",
+    "url": "/yxy-demo-react-app/static/css/main.c04e2125.chunk.css"
+  },
+  {
+    "revision": "e016205d17d9fdadc757",
+    "url": "/yxy-demo-react-app/static/js/2.53ea4124.chunk.js"
+  },
+  {
+    "revision": "dd2bafddf84d0013ad828f5f5d6dbbaf",
+    "url": "/yxy-demo-react-app/static/js/2.53ea4124.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75bac312a74f004faf97",
+    "url": "/yxy-demo-react-app/static/js/main.5855ea56.chunk.js"
+  },
+  {
+    "revision": "90939e75172bc6db3348",
+    "url": "/yxy-demo-react-app/static/js/runtime-main.4c60bad3.js"
+  }
+]);
