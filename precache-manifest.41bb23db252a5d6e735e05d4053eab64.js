ddbdcd4290b031b2b89425a35329fc4c8b353658
Tom Yang <xy2286@columbia.edu>
2020-08-21 15:44:40 -0400

    Updates

--- precache-manifest.41bb23db252a5d6e735e05d4053eab64.js
+++ precache-manifest.41bb23db252a5d6e735e05d4053eab64.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d3394a9fe208d8a5c27e4013de3fcad9",
+    "revision": "4e71433fb94cd8514342d7adc9dbac5d",
     "url": "/yxy-demo-react-app/index.html"
   },
   {
-    "revision": "c399300160ef26162cf3",
+    "revision": "adec9a3a944c77b882e4",
     "url": "/yxy-demo-react-app/static/css/main.c04e2125.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/yxy-demo-react-app/static/js/2.84016699.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c399300160ef26162cf3",
-    "url": "/yxy-demo-react-app/static/js/main.bec6087b.chunk.js"
+    "revision": "adec9a3a944c77b882e4",
+    "url": "/yxy-demo-react-app/static/js/main.ec329034.chunk.js"
   },
   {
     "revision": "90939e75172bc6db3348",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yxy-demo-react-app/precache-manifest.e64a9572f724984a1ca0cafc3f5b1b62.js"
+  "/yxy-demo-react-app/precache-manifest.41bb23db252a5d6e735e05d4053eab64.js"
 );
 
 self.addEventListener('message', (event) => {
